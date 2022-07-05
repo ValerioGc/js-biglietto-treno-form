36738f378871941030a9c1fd45d513b0b7ef2b57
Valerio Genco <valerio.gc1@gmail.com>
2022-07-05 15:39:49 +0200

    Aggiunto event listner submit

--- js/main.js
+++ js/main.js
@@ -1,36 +1,17 @@
-//Dichiaro variabili KM ed Età
-let distance = parseInt(prompt("Quanti Kilometri devi percorrere?"));
-const age = parseInt(prompt("Quanti Anni hai?"));
-console.log(`L'utente ha ${age} anni`);
-console.log(`L'utente deve percorrere ${distance}km`);
-if (isNaN(distance) && isNaN(age)) {
-    alert("Il valore inserito non è un numero! Per favore inserisci correttamente i valori richiesti");
-    location.reload();
-}
-//Calcolo prezzi biglietti
-const km_price = 0.21;
-const full_price_ticket = ((distance * km_price) * 100) / 100;
-const ticket = full_price_ticket.toFixed(2)
-console.log(`Il prezzo intero del biglietto è: ${ticket}€`);
-// Calcolo sconti
 
-let ticket_under = full_price_ticket - ((full_price_ticket * 20) / 100);
-let ticket_d_under= ticket_under.toFixed(2)
-console.log(`Il prezzo under 18 con del biglietto è: ${ticket_d_under}€`);
+let submit = document.getElementById('generate_ticket')
+
+// Event listner Submit 
+submit.addEventListener("click", function () {
+    let name_passenger = document.getElementById('nome').value;
+    console.log(`L'utente si chiama ${name_passenger}`);
+    let distance = document.getElementById('distanza').value;
+    console.log(`L'utente deve percorrere ${distance}km`);
+    let age = document.getElementById('eta').value;
+    console.log(`L'utente rientra nella categoria sconto ${age}`);
+    document.getElementById("generate_ticket").innerHTML = "Rigenera";
+});
 
 
-// Biglietto sconto over
-let ticket_over = full_price_ticket - ((full_price_ticket * 40) / 100);
-let ticket_d_over = ticket_under.toFixed(2)
-console.log(`Il prezzo over 65 del biglietto è: ${ticket_d_over}€`);
-// Output prezzo biglietto del treno
-if (age >= 65) {
-    alert(`Hai diritto ad uno sconto per over 65! Il prezzo del biglietto è di ${ticket_d_over}€`);
-}
-else if (age <= 18) {
-    alert(`Hai diritto ad uno sconto per under 18! Il prezzo del biglietto è di ${ticket_d_under}€`);
-}
-else {
-    alert(`Il prezzo del biglietto è di ${ticket}€`);
-}
+
 
