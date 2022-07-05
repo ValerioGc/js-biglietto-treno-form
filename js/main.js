4b2a4cb2f496a5c7f5cb933f73b70f96fb2cf1a0
Valerio Genco <valerio.gc1@gmail.com>
2022-07-05 15:51:32 +0200

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
-
-let submit = document.getElementById('generate_ticket')
-
+// Definisco variabili bottoni
+const submit = document.getElementById('generate_ticket')
+const refresh = document.getElementById('refresh_page')
 // Event listner Submit 
 submit.addEventListener("click", function () {
+    document.getElementById("generate_ticket").innerHTML = "Rigenera";
+    // Nome Passeggero
     let name_passenger = document.getElementById('nome').value;
     console.log(`L'utente si chiama ${name_passenger}`);
+    // Distanza da percorrere
     let distance = document.getElementById('distanza').value;
     console.log(`L'utente deve percorrere ${distance}km`);
+    // Fascia età Passeggero
     let age = document.getElementById('eta').value;
     console.log(`L'utente rientra nella categoria sconto ${age}`);
-    document.getElementById("generate_ticket").innerHTML = "Rigenera";
+    //Calcolo prezzi biglietti
+    const km_price = 0.21;
+    let full_price_ticket = ((distance * km_price) * 100) / 100;
+    let ticket = full_price_ticket.toFixed(2)
+    console.log(`Il prezzo intero del biglietto è: ${ticket}€`);
+    // Calcolo sconti
+    let ticket_under = full_price_ticket - ((full_price_ticket * 20) / 100);
+    let ticket_d_under = ticket_under.toFixed(2)
+    console.log(`Il prezzo under 18 con del biglietto è: ${ticket_d_under}€`);
+    // Biglietto sconto over
+    let ticket_over = full_price_ticket - ((full_price_ticket * 40) / 100);
+    let ticket_d_over = ticket_under.toFixed(2)
+    console.log(`Il prezzo over 65 del biglietto è: ${ticket_d_over}€`);
 });
-
-
-
+// Event Listner Bottone Annulla
+refresh.addEventListener('click', function () {
+        console.log('Aggiorno Pagina')
+        location.reload()
+});
 
